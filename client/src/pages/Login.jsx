0966d645f6d4529a03331294c00820084fe0bb2f
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-24 11:27:21 +0300

    client login page updated

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,24 +14,22 @@ const Login = () => {
     const user = Object.fromEntries(formData.entries());
 
     api
-    .post("/auth/login", user)
-    .then((res) => {
-      // Bildirim gönder
-      toast.success(res.data.message);
+      .post("/auth/login", user)
+      .then((res) => {
+        // Bildirim gönder
+        toast.success(res.data.message);
 
-      // Kullanıcı bilgilerini locale kaydet
-      localStorage.setItem("user", JSON.stringify(res.data.user));
-      
-      // jvt tokenini local'e kaydet
-      localStorage
-      .setItem("token", res.data.token)
-      
-      // Kullanıcıyı ana sayfaya yönlendir
-      navigate("/");
-      
-    });
+        // Kullanıcı bilgilerini locale kaydet
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+
+        // jvt tokenini local'e kaydet
+        localStorage.setItem("token", res.data.token);
+
+        // Kullanıcıyı ana sayfaya yönlendir
+        navigate("/");
+      })
       .catch((err) => console.log(err));
-    };
+  };
   return (
     <div className="pt-24 max-w-[700px] mx-auto sm:min-w-[400px] max-sm:w-full">
       <h1 className="text-xl md:text-2xl font-bold mb-10 text-gray-500">
